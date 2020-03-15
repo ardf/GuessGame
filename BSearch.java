d0494f94b64b4a6240c031608a92dcbb11c9582a
ardf <60780231+ardf@users.noreply.github.com>
2020-03-15 14:15:34 +0530

    Update BSearch.java

--- BSearch.java
+++ BSearch.java
@@ -1,7 +1,8 @@
 package GuessGame;
-
+//This class contains partial implementation of Binary Search
 public class BSearch {
     public static int mid;
+    //This method returns the middle element from the given set of numbers
     public static int search(int l,int h){
        mid = (l+h)/2;
        return mid;
